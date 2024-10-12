c16bfd2708366b8b3a587a3ed04f09125a5c7f20
BroBiao <brobiao29@gmail.com>
2024-10-13 02:47:37 +0800

    add trend alignment condition again

--- strategy.py
+++ strategy.py
@@ -15,7 +15,7 @@ tz = pytz.timezone('Asia/Shanghai')
 watchlist_path = Config.WL_PATH
 market_base_url = Config.MKT_BASE_URL
 
-debug_mode = True
+debug_mode = False
 if debug_mode == True:
     proxies = {
     'https': 'http://127.0.0.1:7890/',
@@ -35,9 +35,9 @@ def send_message(message):
         asyncio.run(bot.send_message(chat_id=chat_id, text=message))
 
 def run():
-    timeframe = Config.SMALL_TIMEFRAME
-    timeframe_sec = timeframe_to_seconds(timeframe)
-    trend_timeframe = Config.LARGE_TIMEFRAME
+    operate_timeframe = Config.SMALL_TIMEFRAME
+    operate_timeframe_sec = timeframe_to_seconds(operate_timeframe)
+    observe_timeframe = Config.LARGE_TIMEFRAME
 
     all_pairs = read_json_file(watchlist_path)
     for pair in all_pairs:
@@ -46,18 +46,19 @@ def run():
             data = PairData(pair, proxies)
 
             # Main Strategy: Bullish/Bearish Flag Strategy
-            klines = data.get_klines(timeframe)
+            klines = data.get_klines(operate_timeframe)
             window_width = Config.MAXMIN_WINDOW_WIDTH
-            oc_max = data.get_oc_max(timeframe)
+            oc_max = data.get_oc_max(operate_timeframe)
             local_peaks_index = data.get_peaks(oc_max, window_width)
             local_peaks = [(klines['opentime'][i], oc_max[i]) for i in local_peaks_index]
 
-            oc_min = data.get_oc_min(timeframe)
+            oc_min = data.get_oc_min(operate_timeframe)
             oc_min_neg = [-each for each in oc_min]
             local_valleys_index = data.get_peaks(oc_min_neg, window_width)
             local_valleys = [(klines['opentime'][i], oc_min[i]) for i in local_valleys_index]
 
-            ema_trend = data.check_ema(trend_timeframe)
+            operate_trend = data.check_ema(operate_timeframe)
+            observe_trend = data.check_ema(observe_timeframe)
 
             # Check Bullish Flag & Descending Channel Pattern
             if local_peaks[-2][1] > local_peaks[-1][1]:
@@ -72,20 +73,23 @@ def run():
                     else:
                         break
                 pair_market_url = market_base_url + pair
-                if (oc_max[-2] < local_peaks[-1][1]) and (oc_max[-1] >= local_peaks[-1][1]) and (ema_trend != 'SHORT'):
+                if ((oc_max[-2] < local_peaks[-1][1]) and (oc_max[-1] >= local_peaks[-1][1]) and 
+                    (operate_trend == 'LONG') and (observe_trend != 'SHORT')):
                     message2send = f'\U0001F42E\U0001F4C8 {pair}\n' + flag_message + pair_market_url
                     send_message(message2send)
+                '''
                 if local_valleys[-2][1] >= local_valleys[-1][1]:
                     peak_trend_slope = (local_peaks[-2][1] - local_peaks[-1][1])/(local_peaks[-2][0] - local_peaks[-1][0])
                     peak_trend_value = local_peaks[-1][1] + peak_trend_slope * (klines['opentime'][-1] - local_peaks[-1][0])
                     valley_trend_slope = (local_valleys[-2][1] - local_valleys[-1][1])/(local_valleys[-2][0] - local_valleys[-1][0])
                     if ((peak_trend_slope <= valley_trend_slope) and (klines['high'][-2] < peak_trend_value) and 
-                        (klines['high'][-1] >= peak_trend_value) and (ema_trend != 'LONG')):
+                        (klines['high'][-1] >= peak_trend_value) and (observe_trend != 'LONG')):
                         flag_message += f'L2: %s  %f\nL1: %s  %f\n' % (
                                         str(dt.datetime.fromtimestamp(local_valleys[-2][0]/1000, tz)), local_valleys[-2][1],
                                         str(dt.datetime.fromtimestamp(local_valleys[-1][0]/1000, tz)), local_valleys[-1][1])
                         message2send = f'\U00002197\U00002198 {pair}\n' + flag_message + pair_market_url
                         send_message(message2send)
+                '''
 
             # Check Bearish Flag & Ascending Channel Pattern
             if local_valleys[-2][1] < local_valleys[-1][1]:
@@ -100,22 +104,23 @@ def run():
                     else:
                         break
                 pair_market_url = market_base_url + pair
-                if (oc_min[-2] > local_valleys[-1][1]) and (oc_min[-1] <= local_valleys[-1][1]) and (ema_trend != 'LONG'):
+                if ((oc_min[-2] > local_valleys[-1][1]) and (oc_min[-1] <= local_valleys[-1][1]) and 
+                    (operate_trend == 'SHORT') and (observe_trend != 'LONG')):
                     message2send = f'\U0001F43B\U0001F4C9 {pair}\n' + flag_message + pair_market_url
                     send_message(message2send)
+                '''
                 if local_peaks[-2][1] <= local_peaks[-1][1]:
                     valley_trend_slope = (local_valleys[-2][1] - local_valleys[-1][1])/(local_valleys[-2][0] - local_valleys[-1][0])
                     valley_trend_value = local_valleys[-1][1] + valley_trend_slope * (klines['opentime'][-1] - local_valleys[-1][0])
                     peak_trend_slope = (local_peaks[-2][1] - local_peaks[-1][1])/(local_peaks[-2][0] - local_peaks[-1][0])
                     if ((valley_trend_slope >= peak_trend_slope) and (klines['low'][-2] > valley_trend_value) and 
-                        (klines['low'][-1] <= valley_trend_value) and (ema_trend != 'SHORT')):
+                        (klines['low'][-1] <= valley_trend_value) and (observe_trend != 'SHORT')):
                         flag_message += f'H2: %s  %f\nH1: %s  %f\n' % (
                                        str(dt.datetime.fromtimestamp(local_peaks[-2][0]/1000, tz)), local_peaks[-2][1],
                                        str(dt.datetime.fromtimestamp(local_peaks[-1][0]/1000, tz)), local_peaks[-1][1])
                         message2send = f'\U00002198\U00002197 {pair}\n' + flag_message + pair_market_url
                         send_message(message2send)
-                else:
-                    pass
+                '''
 
         except Exception as e:
             error_message = 'Error Occurred!\n' + pair + ': ' + traceback.format_exc()

308ee1dabd7e0cbd7473044788c788bb2a79e433
BroBiao <brobiao29@gmail.com>
2024-12-11 05:17:42 +0800

    strategy optimization

--- grid.py
+++ grid.py
@@ -46,8 +46,8 @@ def send_message(message):
     loop.run_until_complete(bot.send_message(chat_id=chat_id, text=message))
 
 def format_price(price):
-    """价格抹零，格式化为1000的整数倍"""
-    return int(float(price) // 1000 * 1000)
+    """价格抹零，格式化为priceStep的整数倍"""
+    return int(float(price) // priceStep * priceStep)
 
 def get_balance():
     """获取资产余额"""
@@ -117,44 +117,57 @@ def update_orders(current_price):
     last_trade = get_last_trade(pair)
     last_trade_side = 'BUY' if last_trade['isBuyer'] else 'SELL'
     last_trade_qty = float(last_trade['qty'])
+    last_trade_price = last_trade['price']
 
     # 挂单没有减少，分情况处理
-    if (buy_orders or sell_orders) and (not filled_orders):
-        # 双边均有挂单
-        if buy_orders and sell_orders:
+    if not filled_orders:
+        # 卖单一侧有挂单
+        if sell_orders:
             print('等待挂单成交...')
             return
-        # 只有买单一侧有挂单
-        elif buy_orders and (not sell_orders):
+        # 只有买单一侧有挂单(仓位已清空，追高接货)
+        elif buy_orders:
             if current_price >= (last_refer_price + priceStep):
-                refer_price = format_price(last_refer_price + priceStep)
+                # 风控
+                if current_price < (last_trade_price + 10 * priceStep):
+                    refer_price = (last_refer_price + priceStep)
+                else:
+                    print('价格偏离最近成交价太远，停止挂买单')
+                    return
             else:
                 print('等待挂单成交...')
                 return
-        # 只有卖单一侧有挂单
-        elif (not buy_orders) and sell_orders:
-            if current_price <= (last_refer_price - priceStep):
-                refer_price = format_price(last_refer_price - priceStep)
-            else:
-                print('等待挂单成交...')
-                return
-    # 挂单减少(成交或取消)或无挂单(程序首次启动)
+        # 买卖两侧均无挂单(首次启动)
+        else:
+            refer_price = format_price(last_trade_price)
+    # 挂单减少(成交或取消)
     else:
         # 确认消失的挂单是否成交
         filled_flag = False
         filled_message = ''
+        last_trade_time = 0
         for order in filled_orders:
             order_info = client.get_order(symbol=pair, orderId=int(order))
             # 确认成交，使用最新成交订单的数据
             if order_info['status'] == 'FILLED':
                 filled_flag = True
-                last_trade_side = order_info['side']
-                last_trade_qty = round(float(order_info['executedQty']), quantityDecimals)
-                refer_price = format_price(order_info['price'])
-                filled_message += f"{last_trade_side} {last_trade_qty}{baseAsset} at {refer_price}"
-        # 消失的挂单未成交(比如被取消)，使用最后一次成交的数据
+                filled_trade_side = order_info['side']
+                filled_trade_qty = round(float(order_info['executedQty']), quantityDecimals)
+                filled_trade_price = order_info['price']
+                filled_message += f"{filled_trade_side} {filled_trade_qty}{baseAsset} at {filled_trade_price}"
+                if filled_trade_side == 'BUY'
+                    refer_price = (last_refer_price - priceStep)
+                else:
+                    refer_price = (last_refer_price + priceStep)
+                # 更新最新成交订单数据
+                filled_time = order_info['updateTime']
+                if filled_time > last_trade_time:
+                    last_trade_time = filled_time
+                    last_trade_side = filled_trade_side
+                    last_trade_qty = filled_trade_qty
+        # 消失的挂单未成交(被取消)，挂单参考价保持不变
         if filled_flag == False:
-            refer_price = format_price(last_trade['price'])
+            refer_price = last_refer_price
 
     # 取消剩余挂单
     if open_orders:
@@ -162,9 +175,10 @@ def update_orders(current_price):
 
     # 资金是否全部解锁
     if not wait_asset_unlock(base_balance, quote_balance):
-        send_message("资金未能全部解锁，放弃创建新挂单")
+        send_message("资金尚未全部解锁，无法创建新挂单")
         return
 
+    # 发送成交信息
     if filled_flag:
         send_message(filled_message)
 
@@ -176,7 +190,7 @@ def update_orders(current_price):
     else:
         initial_buy_qty = initialBuyQuantity
 
-    # 买单：往下挂 1000 整数倍的价格
+    # 买单：往下挂 priceStep 整数倍的价格
     for i in range(numOrders):
         buy_price = round(refer_price - (i + 1) * priceStep, priceDecimals)
         buy_qty = round(initial_buy_qty + i * buyIncrement, quantityDecimals)
@@ -189,7 +203,7 @@ def update_orders(current_price):
             buy_orders.append(order['orderId'])
             quote_balance -= (buy_price * buy_qty)
 
-    # 卖单：往上挂 1000 整数倍的价格
+    # 卖单：往上挂 priceStep 整数倍的价格
     for i in range(numOrders):
         sell_price = round(refer_price + (i + 1) * priceStep, priceDecimals)
         if base_balance < sellQuantity:
